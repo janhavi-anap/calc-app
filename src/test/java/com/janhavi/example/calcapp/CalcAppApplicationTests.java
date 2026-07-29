4a733042735d7271e0acec9280fad30485017e67
janhavi-anap <janhavi.anap@oracle.com>
2026-07-29 14:31:36 +0530

    initial git push with base project base functionalities

---
+++ src/test/java/com/janhavi/example/calcapp/CalcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janhavi.example.calcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/janhavi/example/calcapp/service/CalculationTest.java
@@ -0,0 +1,38 @@
+package com.janhavi.example.calcapp.service;
+
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+
+import java.lang.reflect.Method;
+
+public class CalculationTest {
+    Calculation calc;
+    Logger logger = LoggerFactory.getLogger(CalculationTest.class);
+    @BeforeEach
+    public void setUp(){
+        logger.info("setup initiated and service instantiated ");
+        calc=new Calculation();
+    }
+    @Test
+    public void testAdd(){
+        Assertions.assertNotNull(calc, "service object is null");
+        Assertions.assertEquals(9.0,calc.add(3.0,6.0));
+    }
+    @Test
+    public void testSub(){
+        Assertions.assertNotNull(calc, "service object is null");
+        Assertions.assertEquals(4.0,calc.sub(6.0,2.0));
+    }
+    @Test
+    public void testMul(){
+        Assertions.assertNotNull(calc, "service object is null");
+        Assertions.assertEquals(12.0,calc.mul(6.0,2.0));
+    }
+    @AfterEach
+    public  void  tearDown(){
+        calc=null;
+        logger.info("tear down:- initiated and service instance destroyed");
+    }
+}
